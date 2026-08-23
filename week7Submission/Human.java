c8324420a2c9203a708ee8f286eba7111c9242a4
Ryan-Destura <54575158+Ryan-Destura@users.noreply.github.com>
2026-08-23 22:45:55 +1000

    Human v0.1 - Created an enum for ageGroup

--- week7Submission/Human.java
+++ week7Submission/Human.java
@@ -1,29 +1,56 @@
 import java.util.Random;
- 
-class Human implements AgeGroup {
+
+class Human {
     private AgeGroup age;
+    //private double age = 0.1; // placeholder for age group, will be set by the simulation
     private double immunity;
     private boolean infected;
-    private double health; // cannot exceed 0.5
+    private double health; // cannot go outside range 0.99-->0.5
     private double mortality = 0.1;
+
+    //rng modifier to add randomness to health calculation
     Random rand = new Random();
+    double rng = rand.nextDouble(0.4);
+
+    int randomAge = rand.nextInt(4); // generates a random number between 0 and 3
 
     public Human() {
+        if (randomAge == 0) {
+            this.age = AgeGroup.CHILD;
+        } else if (randomAge == 1) {
+            this.age = AgeGroup.YOUTH;
+        } else if (randomAge == 2) {
+            this.age = AgeGroup.ADULT;
+        } else {
+            this.age = AgeGroup.ELDERLY;
+        }
+    }
 
+    public double Age() {
+        return this.age.getAgeValue();
     }
 
     //public infect(); 
-    public recover(); 
-    public move(); // dependent on grid
+    //public void recover(); 
+    //public void move(); // dependent on grid
     public double health(){
-        this.health = 0.4;
+        this.health = Age() + rng;
         return this.health;
+    }
+
+    public void infect(){ 
+        //if this cell has an uninfected neighbour --> range
+        //then infect cell
+
     }
 
     public static void main(String[] args) {
         Random rand = new Random();
-        int any = rand.nextInt(100);
+        Human human = new Human();
+        double any = human.health();
         System.out.println(any);
     }
+
+
 }
 
