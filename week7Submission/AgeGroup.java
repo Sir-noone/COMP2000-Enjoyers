d8991114bb07950962d231fcaa753a31a4a7ab1b
Ryan-Destura <54575158+Ryan-Destura@users.noreply.github.com>
2026-08-23 22:27:35 +1000

    ageGroup test

--- week7Submission/AgeGroup.java
+++ week7Submission/AgeGroup.java
@@ -1,3 +1,17 @@
-interface AgeGroup {
+enum AgeGroup {
     
+    CHILD(0.1),
+    YOUTH(-0.2),
+    ADULT(-0.3),
+    ELDERLY(0.1);
+
+    private final double ageValue;
+
+    AgeGroup(double ageValue) {
+        this.ageValue = ageValue;
+    }
+
+    public double getAgeValue() {
+        return ageValue;
+    }
 }
