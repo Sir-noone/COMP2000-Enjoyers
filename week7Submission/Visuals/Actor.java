d50b1c390bd42477178ba5ecb1065f0091188399
Kaifeng Fan <fankevin170@gmail.com>
2026-08-31 21:16:06 +1000

    Grid v1.3 - Cleaning and streamlining grid code

--- week7Submission/Visuals/Actor.java
+++ week7Submission/Visuals/Actor.java
@@ -48,11 +48,4 @@ public abstract class Actor{
     setPoly();
   }
 
-  public void pulsate(char phase, int percentage) {
-    // Adjust color saturation according to the beat
-    float[] hsbValues = new float[3];
-    Color.RGBtoHSB(baseColor.getRed(), baseColor.getGreen(), baseColor.getBlue(), hsbValues);
-    hsbValues[1] = ((float) percentage) / 100.0f;
-    color = Color.getHSBColor(hsbValues[0], hsbValues[1], hsbValues[2]);
-  }
 }
--- week7Submission/Visuals/ChoosingActor.java
+++ week7Submission/Visuals/ChoosingActor.java
@@ -4,16 +4,17 @@ import java.util.Optional;
 
 public class ChoosingActor implements GameState {
   @Override
+  
   public void mouseClick(int x, int y, Stage s) {
     s.playerInAction = Optional.empty();
     for(Actor player: s.listOfPlayers) {
       if(player.loc.contains(x, y) && !player.isBot()) {
         s.cellOverlay = s.grid.getRadius(player.loc, player.moves);
         s.playerInAction = Optional.of(player);
-        s.currentState = new SelectingNewLocation();
       }
     }
   }
+
 
   @Override
   public void paint(Graphics g, Stage s) {
