0ed14f7fbf5979f0c0ba64644a5352dc225b1317
Ahmed <ahmed.trad@students.mq.edu.au>
2026-09-02 08:53:33 +1000

    V1.3: Added additional descriptions and imported Visuals.

--- week7Submission/TransmissionStrategy.java
+++ week7Submission/TransmissionStrategy.java
@@ -1,18 +1,23 @@
-/**
- * Defines the general behaviour for a tranmission method.
- */
+import Visuals.*;
 
+/**
+ * Defines the general behaviour for a disease tranmission strategy.
+ * 
+ * Different tranmission types can implement this interface using their own rules
+ * for determining whether a disease is successfully tranmitted between two infectable entities.
+ */
 public interface TransmissionStrategy {
 
 /**
  * Attempts to transmit the disease from one infectable entity (human) to another.
+ * 
+ * The tranmission strategy determines whether the source is capable
+ * of tranmissitting the disease and whether the target becomes infected.
  * 
  * @param source the human who may transmit the disease
  * @param target the human who may receive the disease
  * @param disease the disease being transmitted
  * @return true if tranmission succeeds, false otherwise
  */
-
     boolean transmit(Infectable source, Infectable target, Disease disease);
-
 }
